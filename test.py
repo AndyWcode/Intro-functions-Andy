6905a46a459f25275c0de88c88c094fcc0171279
Andy Weng <andyweng257@gmail.com>
2025-09-11 06:41:45 -0700

    Update test.py

--- test.py
+++ test.py
@@ -19,15 +19,62 @@ t.speed(10)
 #         size += 5 
 # loopsquare()
 
-def star(x):
-    for i in range(5):
-        t.forward(x)
-        t.left(144)
 
-def starloop():
-    size = 5
-    for i in range(60):
+#def equalsides(x):
+#     t.left(120)
+#     t.forward(x)
+#     t.left(120)
+#     t.forward(x)
+#     t.left(120)
+#     t.forward(x)
+
+# def rectangle(x, y):
+#     t.forward(x)
+#     t.left(90)
+#     t.forward(y)
+#     t.left(90)
+#     t.forward(x)
+#     t.left(90)
+#     t.forward(y)
+
+# equalsides(90)
+# rectangle(125,100)
+
+# FOR CHALLENGE #3
+
+# def square(x):
+#     for i in range(4):
+#         t.forward(x)
+#         t.left(90)    
+# def loopsquare():
+#     for i in range(60):
+#         square(100)
+#         t.left(5)
+
+# assessment #1
+# def square(x):
+#     for i in range(4):
+#         t.forward(x)
+#         t.left(90)
         
+# def loopsquare():
+#     size = 5
+#     for i in range(60):
+#         square(size)
+#         t.left(5)
+#         size += 5
+        
+# def star(x):
+#     for i in range(5):  
+#         t.forward(x)
+#         t.left(144)
+        
+# def loopstar():
+#     size = 5
+#     for i in range (60):
+#         star(size)
+#         t.left(5)
+#         size +=5
 
     
 
