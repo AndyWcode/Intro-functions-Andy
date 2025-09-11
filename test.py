b0f12cb3febdce58ee7d722f2ea82d346ca637bd
Andy Weng <andyweng257@gmail.com>
2025-09-11 06:37:43 -0700

    Update test.py

--- test.py
+++ test.py
@@ -6,83 +6,28 @@ t = Turtle()
 t.shape('turtle')
 t.speed(10)
 
-# Answer for intro.md
-
-<<<<<<< HEAD
-def equalsides(x):
-    for side in range(3):
-        t.forward(x)
-        t.left(120)
-
-def rectangle(x, y):
-    for side in range(2):
-        t.forward(x)
-        t.left(90)
-        t.forward(y)
-        t.left(90)
-
-def square(x):
-    for square in range(4):
-        
-        t.forward(x)
-        t.left(90)
-        
-turtle.done()
-=======
-# def equalsides(x):
-#     t.left(120)
-#     t.forward(x)
-#     t.left(120)
-#     t.forward(x)
-#     t.left(120)
-#     t.forward(x)
-
-# def rectangle(x, y):
-#     t.forward(x)
-#     t.left(90)
-#     t.forward(y)
-#     t.left(90)
-#     t.forward(x)
-#     t.left(90)
-#     t.forward(y)
-
-# equalsides(90)
-# rectangle(125,100)
-
-# FOR CHALLENGE #3
-
-# def square(x):
-#     for i in range(4):
-#         t.forward(x)
-#         t.left(90)    
-# def loopsquare():
-#     for i in range(60):
-#         square(100)
-#         t.left(5)
-
-# assessment #1
 # def square(x):
 #     for i in range(4):
 #         t.forward(x)
 #         t.left(90)
-        
+
 # def loopsquare():
-#     size = 5
+#     size = 5 
 #     for i in range(60):
 #         square(size)
 #         t.left(5)
-#         size += 5
-        
-# def star(x):
-#     for i in range(5):  
-#         t.forward(x)
-#         t.left(144)
-        
-# def loopstar():
-#     size = 5
-#     for i in range (60):
-#         star(size)
-#         t.left(5)
-#         size +=5
->>>>>>> 26cf3a290ddee2e7fb7b6ae265affd77dc5b5ef8
+#         size += 5 
+# loopsquare()
+
+def star(x):
+    for i in range(5):
+        t.forward(x)
+        t.left(144)
+
+def starloop():
+    size = 
+
+    
+
+turtle.done()
 
